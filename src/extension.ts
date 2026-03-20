749b451e24c2caf40f59a092f97bb7599b707305
johndoe <johndoe@mail.com>
2026-03-20 14:45:13 -0700

    v3.0.5

--- src/extension.ts
+++ src/extension.ts
@@ -1,101 +1,40 @@
-/*
-   Copyright (C) 2018-2021 Gourav Goyal and contributors.
+'use strict';
 
-   This file is part of the Shortcut Menu Bar extension.
+import { commands, ExtensionContext, window } from 'vscode';
 
-   The Shortcut Menu Bar extension is free software: you can redistribute it
-   and/or modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation, either version 3 of the
-   License, or (at your option) any later version.
-
-   The Shortcut Menu Bar extension is distributed in the hope that it will
-   be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License along
-   with the Shortcut Menu Bar extension. If not,see <https://www.gnu.org/licenses/>.
-*/
-
-"use strict";
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-// let fs = require("fs");
-import {
-  commands,
-  Disposable,
-  ExtensionContext,
-  window,
-} from "vscode";
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-  console.log("extension is now active!");
-
-  // rest of code
-  // Step: If simple commands then add to this array
-  let commandArray = [
-    //=> ["name in package.json" , "name of command to execute"]
-
-    [
-      "ShortcutMenuBar.toggleTerminal",
-      "workbench.action.terminal.toggleTerminal",
-    ],
-    ["ShortcutMenuBar.navigateBack", "workbench.action.navigateBack"],
-    ["ShortcutMenuBar.navigateForward", "workbench.action.navigateForward"],
-    [
-      "ShortcutMenuBar.compareWithSaved",
-      "workbench.files.action.compareWithSaved",
-    ],
+  const builtinCommands = [
+    ['ShortcutMenuBar.navigateBack', 'workbench.action.navigateBack'],
+    ['ShortcutMenuBar.navigateForward', 'workbench.action.navigateForward'],
+    ['ShortcutMenuBar.toggleTerminalOn', 'workbench.action.terminal.toggleTerminal'],
+    ['ShortcutMenuBar.toggleTerminalOff', 'workbench.action.togglePanel'],
+    ['ShortcutMenuBar.togglePrimarySideBarOn', 'workbench.action.toggleSidebarVisibility'],
+    ['ShortcutMenuBar.togglePrimarySideBarOff', 'workbench.action.toggleSidebarVisibility'],
+    ['ShortcutMenuBar.toggleSecondarySideBarOn', 'workbench.action.toggleAuxiliaryBar'],
+    ['ShortcutMenuBar.toggleSecondarySideBarOff', 'workbench.action.toggleAuxiliaryBar'],
   ];
 
-  let disposableCommandsArray: Disposable[] = [];
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-
-  commandArray.forEach((command) => {
-    disposableCommandsArray.push(
-      commands.registerCommand(command[0], () => {
-        commands.executeCommand(command[1]).then(function () {});
-      })
+  for (const [name, command] of builtinCommands) {
+    context.subscriptions.push(
+      commands.registerCommand(name, () => {
+        commands.executeCommand(command);
+      }),
     );
-  });
+  }
 
-  // Step: else add complex command separately
-
-  let disposableBeautify = commands.registerCommand(
-    "ShortcutMenuBar.beautify",
-    () => {
-      let editor = window.activeTextEditor;
+  context.subscriptions.push(
+    commands.registerCommand('ShortcutMenuBar.format', () => {
+      const editor = window.activeTextEditor;
       if (!editor) {
-        return; // No open text editor
+        return;
       }
-
-      if (window.state.focused === true && !editor.selection.isEmpty) {
-        commands
-          .executeCommand("editor.action.formatSelection")
-          .then(function () {});
-      } else {
-        commands
-          .executeCommand("editor.action.formatDocument")
-          .then(function () {});
-      }
-    }
+      const command =
+        window.state.focused && !editor.selection.isEmpty
+          ? 'editor.action.formatSelection'
+          : 'editor.action.formatDocument';
+      commands.executeCommand(command);
+    }),
   );
-
-  // Adding 1) to a list of disposables which are disposed when this extension is deactivated
-
-  disposableCommandsArray.forEach((i) => {
-    context.subscriptions.push(i);
-  });
-
-  // Adding 2) to a list of disposables which are disposed when this extension is deactivated
-
-  context.subscriptions.push(disposableBeautify);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
